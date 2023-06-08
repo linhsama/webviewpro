645c67d0b25961e50366efab7e160a75f611b007
linhsama <fdcvlinh6@gmail.com>
2023-06-09 03:23:58 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,168 @@
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
+import 'package:webviewpro/src/menu.dart';
+import 'package:webviewpro/src/navigation_controls.dart';
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: WebViewExample(),
+    ),
+  );
+}
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  final Completer<WebViewController> _controller =
+  Completer<WebViewController>();
+  var loadingPercentage = 0;
+
+  Future<bool> _onPop() async {
+    return (await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: new Text('Xác nhận'),
+        content: new Text('Đóng ứng dụng?'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            //<-- SEE HERE
+            child: new Text('Không'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            // <-- SEE HERE
+            child: new Text('Đóng'),
+          ),
+        ],
+      ),
+    )) ??
+        false;
+  }
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      WillPopScope(
+        onWillPop: _onPop,
+
+        child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text('Flutter WebView'),
+          // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
+          actions: <Widget>[
+            NavigationControls(_controller.future),
+            // SampleMenu(_controller.future),
+          ],
+        ),
+        // We're using a Builder here so we have a context that is below the Scaffold
+        // to allow calling Scaffold.of(context) so we can show a snackbar.
+        body: Builder(builder: (BuildContext context) {
+          return Stack(
+            children:[
+
+
+
+            WebView(
+              initialUrl: 'http://mnkimdong.ddns.net/webview',
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController webViewController) {
+                _controller.complete(webViewController);
+              },
+
+              onPageStarted: (url) {
+                setState(() {
+                  loadingPercentage = 0;
+                });
+                print('WebView is started (url : $url)');
+              },
+              onProgress: (int progress) {
+                setState(() {
+                  loadingPercentage = progress;
+                });
+                print('WebView is loading (progress : $progress%)');
+              },
+              onPageFinished: (url) {
+                setState(() {
+                  loadingPercentage = 100;
+                });
+                print('WebView is finish (url : $url)');
+              },
+
+              javascriptChannels: <JavascriptChannel>{
+                _toasterJavascriptChannel(context),
+              },
+              navigationDelegate: (NavigationRequest request) {
+                if (request.url.startsWith('https://www.youtube.com/')) {
+                  print('blocking navigation to $request}');
+                  return NavigationDecision.prevent;
+                }
+                print('allowing navigation to $request');
+                return NavigationDecision.navigate;
+              },
+              gestureNavigationEnabled: true,
+              backgroundColor: const Color(0x00000000),
+              geolocationEnabled: true, // set geolocationEnable true or not
+            ),
+              if (loadingPercentage < 100)
+                LinearProgressIndicator(
+                  value: loadingPercentage / 100.0,
+                ),
+          ]);
+        }),
+        // floatingActionButton: favoriteButton(),
+    ),
+      );
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          // ignore: deprecated_member_use
+          final messenger = ScaffoldMessenger.of(context);
+          messenger.showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
+  }
+
+  Widget favoriteButton() {
+    return FutureBuilder<WebViewController>(
+        future: _controller.future,
+        builder: (BuildContext context,
+            AsyncSnapshot<WebViewController> controller) {
+          if (controller.hasData) {
+            return FloatingActionButton(
+              onPressed: () async {
+                final String url = (await controller.data!.currentUrl())!;
+                final messenger = ScaffoldMessenger.of(context);
+                messenger.showSnackBar(
+                  SnackBar(content: Text('Favorited $url')),
+                );
+              },
+              child: const Icon(Icons.favorite),
+            );
+          }
+          return Container();
+        });
+  }
+}
---
+++ lib/src/navigation_controls.dart
@@ -0,0 +1,70 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
+
+class NavigationControls extends StatelessWidget {
+  const NavigationControls(this._webViewControllerFuture)
+      : assert(_webViewControllerFuture != null);
+
+  final Future<WebViewController> _webViewControllerFuture;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<WebViewController>(
+      future: _webViewControllerFuture,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> snapshot) {
+        final bool webViewReady =
+            snapshot.connectionState == ConnectionState.done;
+        final WebViewController? controller = snapshot.data;
+        return Row(
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: !webViewReady
+                  ? null
+                  : () async {
+                final messenger = ScaffoldMessenger.of(context);
+                if (await controller!.canGoBack()) {
+                  await controller.goBack();
+                } else {
+                  messenger.showSnackBar(
+                    const SnackBar(content: Text('No back history item')),
+                  );
+                  return;
+                }
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.arrow_forward_ios),
+              onPressed: !webViewReady
+                  ? null
+                  : () async {
+                final messenger = ScaffoldMessenger.of(context);
+                if (await controller!.canGoForward()) {
+                  await controller.goForward();
+                } else {
+                  messenger.showSnackBar(
+                      const SnackBar(content: Text('No forward history item')),
+                  );
+                  return;
+                }
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.replay),
+              onPressed: !webViewReady
+                  ? null
+                  : () {
+                controller!.reload();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
